899717f921103a1ea6d08a74dff0c7cd16fb0789
daphne remali <remalidaphne@gmail.com>
2017-02-08 11:37:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Section from './components/Section';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <h1> HELLO WORLD </h1>
+
+
+        <Section/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Section.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Section extends React.Component {
+
+	render() {
+		return (
+			<div>
+
+				<p>Section</p>
+
+
+			</div>
+			);
+	}
+}
+
+export default Section;
